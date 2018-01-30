1e341a1dd5f4add22d5dc6848931236825963006
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2018-01-30 14:29:08 -0800

    Adds Update controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    resources :challenges
+    resources :chapters
+    #Update Controller
+
+    get "/update/connection", to: "update#check_connection", as: "connection"
+    get "/update/repo_availability", to: "update#repo_availability", as: "availability"
+    get "/update/pull", to: "update#pull", as: "pull_repo"
+    post "/update/sync", to: "update#sync", as: "sync"
+
+
 end
