7fae5473bc75470b68a41b372cc3afd777804ad2
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2018-01-30 15:16:49 -0800

    Check for github availability and pull from freecodecamp repo

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -1,2 +1,18 @@
 class UpdateController < ApplicationController
+
+  def repo_availability
+    return system 'ping -c 1 github.com'
+  end
+
+  def pull
+    location = params[:location]
+    system "cd #{location}"
+    return system 'git pull'
+  end
+
+  def sync
+  end
+
+
+
 end
